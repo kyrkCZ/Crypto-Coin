aa23935969108933a586246d288de2a255573f37
Jakub Kramný <57757403+kyrkCZ@users.noreply.github.com>
2023-10-24 14:27:48 +0200

    Uprade to new Gradle build Update to target API 33 Migrate to androidx

--- app/src/main/java/com/jakub/coin/CryptoCoin/Coiny.java
+++ app/src/main/java/com/jakub/coin/CryptoCoin/Coiny.java
@@ -2,7 +2,7 @@ package com.jakub.coin.CryptoCoin;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.jakub.coin.CryptoCoin.data.Data;
